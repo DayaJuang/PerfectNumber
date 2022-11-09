b19c72a8a272da259f9c0d54219a90e95a85a186
daya <dayajuangmahaputra@gmail.com>
2022-11-09 20:45:17 +0900

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        boolean ans = isPerfectNumber(6);
+        System.out.println(ans);
+    }
+
+    //A perfect number is a positive integer which is equal to the sum of its proper positive divisors.
+    public static boolean isPerfectNumber(int n){
+        // Check if n is greater or equal 1
+        if(n < 1)
+            return false;
+
+        // Do the perfect number check
+        // Create the sum variable of the divisor and the divisor itself
+        int sum = 0;
+        int divisor = 1;
+
+        while (divisor < n){
+            // Check if it proper divisor or not (not leaving any remainder)
+            if(n % divisor == 0)
+                sum += divisor; // sum all proper divisor
+            divisor++;
+        }
+
+        // Check if the sum and n is same value
+        return (sum == n);
     }
 }
